0bb3f8f86301cdc1f81199d4c3cf5157c3f69288
Reatlilwe Maroga <rea.maroga@gmail.com>
2024-09-01 03:18:10 +0200

    edited formats

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 const storeItems = {
     1: { id: 1, name: "Always Maxi Duo Sanitary Pads Super Plus x 18", price: 53.95 + (53.95 * 0.4), image: "src/images/always pad pack.jpeg" },
     2: { id: 2, name: "Lil-lets Tampons x32", price: 54.99 + (54.99 * 0.4), image: "src/images/lilets tampons.jpeg" },
-    3: { id: 3, name: "Always Sensitive Sanitary Pads Ultra Super Plus", price: 52.95 + (52.95 * 0.4), image: "src/images/Always Sensitive Sanitary Pads Ultra Super Plus.webp" },
+    3: { id: 3, name: "Always Sensitive Sanitary Pads Ultra Super Plus", price: 52.95 + (52.95 * 0.4), image: "src/images/Always Sensitive Santary Pads Ultra Super Plus.webp" },
     4: { id: 4, name: "PURA Health Alcohol Wipes Hand Sanitizer", price: 32.50 + (32.50 * 0.4), image: "src/images/Alcohol Wipes Hand Sanitizer.webp" },
     5: {id: 5, name:"Dettol Hand Sanitizer", price:86.45+(86.45*0.4), image:"src/images/Hand Sanitizer.webp"}
 };
